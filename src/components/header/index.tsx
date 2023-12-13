3641b63db5a27ead6567c2ed530bf46ff23bfad1
Andrii Trybynenko <trybynenko@gmail.com>
2023-12-13 09:59:19 +0100

    swipe debounce

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,6 +3,7 @@ import {signal} from '@preact/signals';
 import {useEffect, useRef} from 'preact/hooks';
 import {fetchFileContent} from './fetchUtils';
 import Hammer from 'hammerjs';
+import { debounce } from 'lodash';
 
 // Define signals outside the component
 const fileContent = signal('');
@@ -14,49 +15,58 @@ fetchFileContent(fileContent, loading, error);
 
 const regex = /#+/g;
 const columns = 3;
+
 const Header = () => {
-		const containerRef = useRef(null);
-		if (loading.value) return <p>Loading...</p>;
-		if (error.value) return <p>Error loading file: {error.value}</p>;
+	const containerRef = useRef(null);
+	if (loading.value) return <p>Loading...</p>;
+	if (error.value) return <p>Error loading file: {error.value}</p>;
 
-		const text = fileContent.value.replaceAll(regex,'<br>');
-		const words = text.split(' ');
-		const wordsPerColumn = Math.ceil(words.length / columns);
+	const text = fileContent.value.replaceAll(regex,'<br>');
+	const words = text.split(' ');
+	const wordsPerColumn = Math.ceil(words.length / columns);
 
-		useEffect(() => {
-			if (containerRef.current) {
-				for (let i = 0; i < columns; i++) {
-					const columnText = words.slice(i * wordsPerColumn, (i + 1) * wordsPerColumn).join(' ');
-					// Create a new div and add the text
-					const columnDiv = document.createElement('div');
-					columnDiv.classList.add('scroll-item');
-					columnDiv.textContent = columnText;
-					const hammer = new Hammer(columnDiv);
-					hammer.on('panleft', (e) => {
-						// Scroll the element to the right
-						e.target.parentElement.scrollLeft += 360;
-					});
-					hammer.on('panright', (e) => {
-						// Scroll the element to the left
-						e.target.parentElement.scrollLeft -= 360;
-					});
-					containerRef.current.appendChild(columnDiv);
-				}
+	const panleft = (e) => {
+		// Scroll the element to the right
+		e.target.parentElement.scrollLeft += 400;
+		console.log('left', e.target.parentElement.scrollLeft)
+	}
+	const panright = (e) => {
+		// Scroll the element to the left
+		e.target.parentElement.scrollLeft -= 400;
+		console.log('right', e.target.parentElement.scrollLeft)
+	}
+
+	const debouncedHandlePanLeft = debounce(panleft, 200);
+	const debouncedHandlePanRight = debounce(panright, 200);
+
+	useEffect(() => {
+		if (containerRef.current) {
+			containerRef.current.scrollLeft = 0;
+			for (let i = 0; i < columns; i++) {
+				const columnText = words.slice(i * wordsPerColumn, (i + 1) * wordsPerColumn).join(' ');
+				// Create a new div and add the text
+				const columnDiv = document.createElement('div');
+				columnDiv.classList.add('scroll-item');
+				columnDiv.textContent = columnText;
+				const hammer = new Hammer(columnDiv);
+				hammer.on('panleft', debouncedHandlePanLeft);
+				hammer.on('panright', debouncedHandlePanRight);
+				containerRef.current.appendChild(columnDiv);
 			}
-		},[])
+		}
+	},[])
 
-		return (
-			<div>
-				<p>File Content:</p>
-				<div id="app">
-					<main>
-						<div class="scroll-container" ref={containerRef}>
-						</div>
-					</main>
-				</div>
+	return (
+		<div>
+			<p>File Content:</p>
+			<div id="app">
+				<main>
+					<div class="scroll-container" ref={containerRef}>
+					</div>
+				</main>
 			</div>
-);
-}
-;
+		</div>
+	);
+};
 
 export default Header;
